93a8d2bb14424e0a45598f5e73f61c92c7adda26
thintv <thintv@vccloud.vn>
2022-04-05 17:36:45 +0700

    shjt code

--- app/routes/resource.py
+++ app/routes/resource.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, jsonify, current_app
+from flask import Blueprint, g, jsonify, current_app, request
 
 from app.models import Resources
 
@@ -15,3 +15,79 @@ def show(resource_id):
     resource = g.session.query(Resources).filter_by(id=resource_id).one_or_none()
     current_app.oso.authorize(g.current_user, "allow", resource)
     return resource.repr()
+
+
+@bp.route("/create-permission", methods=["POST"])
+def create():
+    payload = request.get_json()
+    if "name" and "description" and "resource" and "project" in payload:
+        resources = payload["resource"]
+        listEndpointRaw = []
+        listEndpoint = []
+        for resource in resources:
+            try:
+                print(resource)
+                if resource.split(".")[0] == "*":
+                    key = f"%.{resource.split('.')[1]}.{resource.split('.')[2]}"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                if resource.split(".")[1] == "*":
+                    key = f"{resource.split('.')[0]}%{resource.split('.')[2]}"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                if resource.split(".")[2] == "*":
+                    key = f"{resource.split('.')[0]}.{resource.split('.')[1]}.%"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                if resource.split(".")[0] == "*" and resource.split(".")[1] == "*":
+                    key = f"%.{resource.split('.')[2]}"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                if resource.split(".")[0] == "*" and resource.split(".")[2] == "*":
+                    key = f"%.{resource.split('.')[1]}.%"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                if resource.split(".")[1] == "*" and resource.split(".")[2] == "*":
+                    key = f"{resource.split('.')[0]}.%"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                if resource.split(".")[0] == "*" and resource.split(".")[1] == "*" and resource.split(".")[2] == "*":
+                    key = f"%"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                if resource == "*":
+                    key = f"%"
+                    endpoints = Resources.query.filter(Resources.endpoint.like(key)).all()
+                    print("in *")
+                    for endpoint in endpoints:
+                        listEndpointRaw.append(endpoint.repr())
+                else:
+                    endpoint = Resources.query.filter_by(endpoint=resource).first()
+                    if endpoint:
+                        listEndpointRaw.append(endpoint.repr())
+            except:
+                pass
+
+        for item in listEndpointRaw:
+            if item not in listEndpoint:
+                listEndpoint.append(item)
+
+        return jsonify({
+            "message": "Success",
+            "status": 1,
+            "status_code": 201,
+            "resource": listEndpoint
+        }), 201
+
+    return jsonify({
+        "message": "Invalid payload",
+        "status": 0,
+        "status_code": 400
+    }), 400
